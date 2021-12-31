9f96e5ea5eb477c9bd95387b0a0423872a6f927e
SterrenHeer <sterrenheer@gmail.com>
2021-12-31 10:11:47 +0300

    Switching to general views

--- survey/views.py
+++ survey/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    return render(request, 'survey/index.html', {'latest_question_list': latest_question_list})
+class IndexView(generic.ListView):
+    template_name = 'survey/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'survey/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'survey/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'survey/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'survey/results.html'
 
 
 def vote(request, question_id):
