c31aa8317e27dfef5ac87f53b231aaee5af46571
SterrenHeer <sterrenheer@gmail.com>
2021-12-31 09:56:34 +0300

    Add some views and templates

--- survey/views.py
+++ survey/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'survey/index.html', {'latest_question_list': latest_question_list})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'survey/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'survey/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'survey/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('survey:results', args=(question.id,)))
